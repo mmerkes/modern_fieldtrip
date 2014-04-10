4468f8304efb2a3ec3c02cc2dba6df1968aa043b
Empty Arsenal <matt.merkes@gmail.com>
2014-04-10 16:48:05 -0700

    Deactivated unfinished pages

--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,9 @@
-App.Router.map(function() {
+App.Router.map(function() {/*
   this.resource('trips', function() {
     this.resource('trip', {path: '/:id'}, function() {
       this.route('edit');
     });
   });
   this.route('login');
-  this.route('signup');
+  this.route('signup');*/
 });
